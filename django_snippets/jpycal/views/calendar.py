94b0424b4a7d40ac0b25c7a4f3413bd8512787cc
Mark Donald <mark@skagos.com.au>
2012-01-03 18:20:03 +1100

    tidy up doco

--- django_snippets/jpycal/views/calendar.py
+++ django_snippets/jpycal/views/calendar.py
@@ -1,3 +1,10 @@
+"""
+Calendar page renderer for jpycal.
+Author: Mark Donald <mark@skagos.com.au>
+
+All else is done with AJAX.
+
+"""
 from django.shortcuts import render_to_response
 
 def get(request):
--- django_snippets/urls.py
+++ django_snippets/urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls.defaults import patterns, include
 
 urlpatterns = patterns('',
     (r'^jpycal', include('django_snippets.jpycal.urls')),
