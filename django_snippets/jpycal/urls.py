333622f15a454af09429930236d54b1d0e705edb
Mark Donald <mark@skagos.com.au>
2012-01-04 02:42:01 +1100

    sub-moduled jpycal and nuthouse

--- django_snippets/jpycal/urls.py
+++
@@ -1,7 +0,0 @@
-from django.conf.urls.defaults import patterns
-
-urlpatterns = patterns('django_snippets.jpycal.views',
-   ('^/events', 'events.dispatch'),
-   (r'^/event/(?P<id>\d+)', 'events.dispatch'),
-   ('^', 'calendar.get'),
-)
